4f0f34ba6ffad1c3355533f5466214ed12ee962f
Mathieu <math.lechat@gmail.com>
2026-03-31 11:24:45 +0200

    Update everything

--- scripts/check-links.mjs
+++ scripts/check-links.mjs
@@ -7,35 +7,33 @@ for (const file of await readdir("source", {withFileTypes: true})) {
         continue;
     }
 
-    reports.push(checkPage(file.name)
-        .then(async redirections => {
-            const report = {
-                page: file.name,
-                redirections: new Map(),
-                errors: [],
-            }
+    reports.push(new Promise(async (resolve) => {
+        const report = {
+            page: file.name,
+            redirections: new Map(),
+            errors: new Map(),
+        }
 
-            for (const [href, redirectionPromise] of redirections) {
-                try {
-                    const redirection = await redirectionPromise
+        for await (const uri of yieldUrls(file.name)) {
+            try {
+                const redirection = await getRedirection(uri);
 
-                    if (redirection) {
-                        report.redirections.set(href, redirection)
+                if (redirection) {
+                    report.redirections.set(uri, redirection)
 
-                        process.stdout.write('\x1b[1m\x1b[33mR\x1b[0m️')
-                    } else {
-                        process.stdout.write('.')
-                    }
-                } catch (e) {
-                    report.errors.push(e)
-
-                    process.stdout.write('\x1b[1m\x1b[31mE\x1b[0m️')
+                    process.stdout.write('\x1b[1m\x1b[33mR\x1b[0m️')
+                } else {
+                    process.stdout.write('.')
                 }
-            }
+            } catch (e) {
+                report.errors.set(uri, e)
 
-            return report;
-        })
-    )
+                process.stdout.write('\x1b[1m\x1b[31mE\x1b[0m️')
+            }
+        }
+
+        resolve(report)
+    }));
 }
 
 Promise.all(reports)
@@ -45,7 +43,7 @@ Promise.all(reports)
         let redirectionCount = 0, errorCount = 0
         for (const report of reports) {
             redirectionCount += report.redirections.size
-            errorCount += report.errors.length
+            errorCount += report.errors.size
         }
 
         if (!errorCount && !redirectionCount) {
@@ -61,13 +59,13 @@ Promise.all(reports)
         console.log()
 
         for (const report of reports) {
-            if (!report.errors.length && !report.redirections.size) {
+            if (!report.errors.size && !report.redirections.size) {
                 continue
             }
 
             console.group(`\x1b[4m${report.page}\x1b[0m`)
-            for (const error of report.errors) {
-                console.log('\x1b[31m●\x1b[0m %s', error)
+            for (const [href, error] of report.errors) {
+                console.log('\x1b[31m●\x1b[0m %s (%s)', error, href)
             }
             for (const [src, dest] of report.redirections) {
                 console.log('\x1b[33m●\x1b[0m %s redirects to %s', src, dest)
@@ -78,23 +76,19 @@ Promise.all(reports)
     })
     .then(() => process.exit())
 
-async function checkPage(page) {
-    const redirections = new Map()
-
-    const html = await readFile(`source/${page}`, "utf8")
-
-    for (const [, href] of html.matchAll(/<a [^>]*href="((?=https?:)[^"]+)/g)) {
-        redirections.set(href, getRedirection(href))
+async function* yieldUrls(page) {
+    for (const [, href] of (await readFile(`source/${page}`, "utf8")).matchAll(/<a [^>]*href="((?=https?:)[^"]+)/g)) {
+        yield href;
     }
-
-    return redirections
 }
 
 async function getRedirection(url) {
-    const response = await fetch(url)
+    const response = await fetch(url, {
+        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:149.0) Gecko/20100101 Firefox/149.0'},
+    })
 
     if (!response.ok) {
-        throw `Request to “${url}“ yielded “${response.status} ${response.statusText}“.`
+        throw `${response.status} ${response.statusText}`
     }
 
     const urlObject = new URL(url)
